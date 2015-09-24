ce7d0a4d336fdd363bdfff261b47b55918720a2d
asinghamgoodwin <asinghamgoodwin@gmail.com>
2015-09-24 13:03:11 -0400

    adding in comments

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from views import app
 
 DATABASE = 'salad.db'
 
+# just a helper function for get_db()
 def connect_to_database():
     return sqlite3.connect(DATABASE)
 
@@ -13,6 +14,12 @@ def get_db():
         db = g._database = connect_to_database()
     return db
 
+# this function is only to be used once to initialize a database from our schema
+# Step 1: delete any existing .db file
+# Step 2: run python
+# Step 3: import models (this file)
+# Step 4: type in models.init_db()
+# Step 5: check to make sure that you now have a salad.db file! you can play with in in sqlite3
 def init_db():
     with app.app_context():
         db = get_db()
@@ -25,3 +32,8 @@ def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
+# HOW TO play with a sqlite3 database in the terminal:
+# Step 1: type in sqlite3 salad.db
+# .tables will give you a list of all of the tables
+# SELECT * FROM Table; will give you a visual of whats in that table
--- views.py
+++ views.py
@@ -7,9 +7,12 @@ from wtforms.validators import DataRequired, Optional
 from models import *
 
 app = Flask(__name__)
+# this is referencing our config.py file, and weirdly lets us get away with not using a secret key
 app.config.from_object("config")
 
 class SaladForm(Form):
+    ### for some reason this didn't work up here so we did the work down in index() where we'd
+    ### already opened a database connection. Maybe we can't open two at once?
     # with app.app_context():
     #     db = get_db()
     #     with db:
@@ -18,25 +21,31 @@ class SaladForm(Form):
     #         categoryList = cur.fetchall()
     
     name = StringField('name', validators=[DataRequired()])
+    # doing validators=[DataRequired()] did not work for the SelectField
     category = SelectField('category', validators=[Optional()])
     ingredient = StringField('ingredient', validators=[DataRequired()])
 
+    # this is helpful because we want to get our category choices from the database
+    # if your choices were static, you could do: SelectFeild('category', choices=[(0,'greens'),(1,'veggies')], validators=...)
     def add_category_choices(self, category_tuples):
         self.category.choices = category_tuples
 
 @app.route('/')
+# the GET is important to display the form and stuff, and the POST lets us grab the form input
 @app.route('/index', methods = ["GET", "POST"])
 def index():
+    # opening a connection to our database (get_db comes from models)
     db = get_db()
     with db:
         cur = db.cursor()
         form = SaladForm()
-        pretend_ingredients = ["cats", "spinace", "avodabo", "carobs"]
-        pretend_categories = ["greebs", "vebebbggeez", "FROOBs"]
 
+        ### this section is to get the categories from our category table to use as drop-down choices
         cur.execute('SELECT category from category')
         categoryList = cur.fetchall()
-        category_tuples = [(x,x) for x in categoryList]
+        # the categories come as tuples, but we only want the first part, x[0]
+        # these come as unicode strings, and we can .encode('utf8') to make them pretty, normal strings
+        category_tuples = [(x[0].encode('utf8'),x[0].encode('utf8')) for x in categoryList]
         form.add_category_choices(category_tuples)
 
         if form.validate_on_submit():
@@ -44,20 +53,28 @@ def index():
             cur.execute('insert into Ingredient (person, category, ingredient) values (?, ?, ?)', 
                         [form.name.data, form.category.data, form.ingredient.data])
             
-            print categoryList
-            print form.name.data
-            print form.category.data
+            # print categoryList
+            # print form.name.data
+            # print form.category.data
         else:
             print "I didn't validate"
+    # could we also use close_connection() ?
     db.close()
+
+    # pretend_ingredients = ["cats", "spinace", "avodabo", "carobs"]
+    # pretend_categories = ["greebs", "vebebbggeez", "FROOBs"]
     return render_template('index.html',
-            form=form,
-            ingredient_list=pretend_ingredients,
-            categories=pretend_categories)
+            form=form)
+            # ingredient_list=pretend_ingredients,
+            # categories=pretend_categories)
 
 
-def get_categories():
-    pass
+# def get_categories():
+#     pass
 
+
+# this is what makes our app start up when we run python views.py
+# I'm pretty sure that the debug=True option makes it so that we don't need to
+# restart the server each time we make a change to one of these files
 if __name__ == '__main__':
     app.run(debug=True)

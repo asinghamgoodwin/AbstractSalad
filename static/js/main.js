c4f1ac7d6d7d1692a89038d7b98b305975917073
Linz Jax <linzjax@gmail.com>
2015-10-08 16:15:30 -0400

    Mobile version is working! Yay!
    Need to add extra functionality to the form display. Currently there is no indication that is what you need to do, and I need to figure out a way to have the form display when a block is selected, and disappear when the block is clicked outside of.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,28 @@
-main.js
+//get the box that was clicked on.
+//remove is_hidden from that form
+var clickEvents = function(){
+
+	var categories = ["Greens","Veggies","Protein","Dressing","Other"];
+
+	categories.forEach(function(cat){
+		// document.getElementById("saladBlock" + cat).addEventListener('click', function(e){
+
+		// 	var form = document.getElementById('saladForm' + cat).attributes['class'].value;
+
+		// 	if(form === 'is_hidden')
+		// 		document.getElementById('saladForm' + cat).attributes.removeNamedItem("class");
+		// });
+
+
+
+		$('#saladBlock'+cat).on('click', function(e){
+			$('#saladForm'+cat).removeClass('is_hidden');
+
+			
+		});
+		
+	});
+};
+
+
+document.addEventListener('load', clickEvents());

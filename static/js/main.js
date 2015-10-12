456347bdc3e1db1c917884b46b34c1810fc4bd8e
Linz Jax <linzjax@gmail.com>
2015-10-12 15:19:58 -0400

    Responsive design complete!
    Next steps: figure out why salad math is no longer working...

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,8 @@ var clickEvents = function(){
 
 	var categories = ["Greens","Veggies","Protein","Dressing","Other"];
 
+	var prev;
+
 	categories.forEach(function(cat){
 		// document.getElementById("saladBlock" + cat).addEventListener('click', function(e){
 
@@ -12,15 +14,28 @@ var clickEvents = function(){
 		// 	if(form === 'is_hidden')
 		// 		document.getElementById('saladForm' + cat).attributes.removeNamedItem("class");
 		// });
-
-
-
-		$('#saladBlock'+cat).on('click', function(e){
-			$('#saladForm'+cat).removeClass('is_hidden');
-
-			
-		});
 		
+
+		$(document).on('click','#saladBlock' + cat, function(e){
+
+			e.stopPropagation();
+
+			if (prev) {
+				prev.addClass('is_hidden');
+				console.log(prev);
+			}
+			var len = this.childNodes.length;
+			console.log(this.childNodes[len - 2]);
+			$('#saladForm' + cat).removeClass('is_hidden');
+			prev = $("#saladForm" +cat);
+
+		});
+
+		$(document).on('click', function(e){
+			$('#saladForm' + cat).addClass('is_hidden');
+		});
+
+
 	});
 };
 
